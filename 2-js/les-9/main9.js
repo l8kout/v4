fe96551a381083666d9e783d2f87fb1c4576dd0e
l8kout <sqet123@gmail.com>
2023-03-09 15:07:55 +0200

    les-9

---
+++ 2-js/les-9/main9.js
@@ -0,0 +1,297 @@
+// // 1 - створити блок,
+// //     - додати йому класи wrap, collapse, alpha, beta
+// let div = document.createElement('div');
+// div.classList.add('wrap','collapse', 'alpha', 'beta');
+//
+// // 2 - додати стилі(довільні значення) : колір фону, колір тексту, розмір тексту
+// div.style.background = 'lightblue'
+// div.innerText = 'hello';
+// div.style.color = 'white';
+// div.style.fontSize = '40px';
+//
+// // 3 - додати цей блок в body.
+// document.body.appendChild(div);
+
+// 4 - клонувати його повністю, та додати клон в body.
+
+// 5 - Є масив:
+//     ['Main','Products','About us','Contacts']
+// Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
+//
+// let arr = ['Main', 'Products', 'About us', 'Contacts'];
+// let ul = document.createElement('ul');
+//
+// for (const arrEl of arr) {
+//     let lii = document.createElement('li');
+//     lii.innerText = arrEl;
+//     ul.appendChild(lii)
+// }
+//
+// document.body.appendChild(ul);
+
+// // 6 - Є масив
+// // let coursesAndDurationArray = [
+// //     {title: 'JavaScript Complex', monthDuration: 5},
+// //     {title: 'Java Complex', monthDuration: 6},
+// //     {title: 'Python Complex', monthDuration: 6},
+// //     {title: 'QA Complex', monthDuration: 4},
+// //     {title: 'FullStack', monthDuration: 7},
+// //     {title: 'Frontend', monthDuration: 4}
+// // ];
+// // Для кожного елементу масиву зробити блок в якому вивести інформацію про title та monthDuration
+// // Завдання робити через цикли.
+//
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
+
+for (const element of coursesAndDurationArray) {
+    let div = document.createElement('div');
+    let text = document.createElement('p');
+    text.innerText = element.title + ' - ' + element.monthDuration;
+    div.appendChild(text);
+    document.body.appendChild(div);
+}
+
+// // 7 - Є масив
+// // let coursesAndDurationArray = [
+// //     {title: 'JavaScript Complex', monthDuration: 5},
+// //     {title: 'Java Complex', monthDuration: 6},
+// //     {title: 'Python Complex', monthDuration: 6},
+// //     {title: 'QA Complex', monthDuration: 4},
+// //     {title: 'FullStack', monthDuration: 7},
+// //     {title: 'Frontend', monthDuration: 4}
+// // ];
+// //
+// //
+// // За допомоги скріпта для кожного елементу масиву зробити <div class='item'> ,  в якому буде <h1 class='heading'>  з title  елементу, та <p class='description'> з monthDuration елементу.
+// //     Завдання робити через цикли.
+//
+// let coursesAndDurationArray = [
+//     {title: 'JavaScript Complex', monthDuration: 5},
+//     {title: 'Java Complex', monthDuration: 6},
+//     {title: 'Python Complex', monthDuration: 6},
+//     {title: 'QA Complex', monthDuration: 4},
+//     {title: 'FullStack', monthDuration: 7},
+//     {title: 'Frontend', monthDuration: 4}
+// ];
+//
+// for (const element of coursesAndDurationArray) {
+//     let div = document.createElement('div');
+//     div.classList.add('item');
+//     let h1 = document.createElement('h1');
+//     h1.classList.add('heading');
+//     h1.innerText = element.title;
+//     let pe = document.createElement('p');
+//     pe.classList.add('description');
+//     pe.innerText = element.monthDuration;
+//     div.appendChild(h1);
+//     div.appendChild(pe);
+//     document.body.appendChild(div);
+// }
+// ==========================
+
+// // 8 - є масив simpsons, котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+// // Проітерувати його, створиши для кожного об'єкту  масиву <div class='member'> та наповнити його данними з об'єкту.
+// //     Якщо людською мовою: під кожного члена родини зробити блок та наповнити його інформацією з цього об'єкту
+// //
+// let simpsons = [
+//     {
+//         name: 'Bart',
+//         surname: 'Simpson',
+//         age: 10,
+//         info: 'Бартолом\'ю ДжоДжо «Барт» Сімпсон (англ. Bartholomew JoJo «Bart» Simpson) — один із головних героїв мультиплікаційного серіалу Сімпсони. Барт — найстарша дитина Гомера і Мардж Сімпсон. У нього також є дві молодші сестри — Ліса і Меґґі. Барт є втіленням образу бешкетника та посереднього учня у школі. Разом зі своїм батьком Барт є одним із найвідоміших персонажів у цьому серіалі.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png'
+//     },
+//     {
+//         name: 'Homer',
+//         surname: 'Simpson',
+//         age: 40,
+//         info: 'Гомер Джей Сімпсон (англ. Homer Jay Simpson) — один із головних героїв мультсеріалу «Сімпсони». Гомер — грубий і неввічливий батько родини, він має очевидні вади: товстий, лисий і не дуже розумний. Нерідко він поводиться як блазень, абсурдно, егоїстично і нетактовно, але все ж лишається симпатичним.',
+//         photo: 'http://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'
+//     },
+//     {
+//         name: 'Marge',
+//         surname: 'Simpson',
+//         age: 38,
+//         info: 'Ма́рджори Жакли́н «Мардж» Си́мпсон (в девичестве Бувье́) (англ. Marjorie Jacqueline «Marge» Simpson) — постоянный персонаж мультипликационного сериала «Симпсоны», её озвучивает Джулия Кавнер. Обычно носит зелёное платье, красные балетки, на шее — ожерелье из искусственного жемчуга и ездит на оранжевом универсале. У неё шикарные синие волосы, которые она обычно собирает в очень высокую причёску. Глаза цвета ореха (19s6e). Основное занятие — домохозяйка, большую часть времени проводит в заботе о доме, детях и Гомере. Образ Мардж копирует стереотип провинциальной американской домохозяйки 50-х годов. Мардж — единственный член семьи, посещающий церковь добровольно. Старается поддерживать нравственность не только своей семьи, но и всего города. Отлично готовит, особенно славятся её свиные отбивные и зефир. Любимое блюдо — лапша с маслом.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'
+//     },
+//     {
+//         name: 'Lisa',
+//         surname: 'Simpson',
+//         age: 9,
+//         info: 'Ли́за Мари́ Си́мпсон (англ. Lisa Marie Simpson) — героиня мультипликационного сериала «Симпсоны». Средний ребёнок в семье, восьмилетняя девочка, выделяющаяся среди остальных Симпсонов прежде всего своим умом и рассудительностью.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/ru/e/ec/Lisa_Simpson.png'
+//     },
+//     {
+//         name: 'Maggie',
+//         surname: 'Simpson',
+//         age: 1,
+//         info: 'Ма́ргарет Эвелин «Мэ́гги» Си́мпсон (англ. Margaret Evelyn “Maggie” Simpson) — персонаж мультсериала «Симпсоны». Впервые появилась на телевидении в шоу Трейси Ульман, в короткометражке Good Night (англ.)русск. 19 апреля 1987 года. Мэгги была придумана и разработана карикатуристом Мэттом Грейнингом, пока он ждал встречи с Джеймсом Л. Бруксом. Названа в честь младшей сестры Грейнинга. После появления в шоу Трейси Ульман, через три года семья Симпсонов получила собственный сериал на телеканале Fox, дебют произошёл 17 декабря 1989 года.',
+//         photo: 'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'
+//     },
+// ];
+//
+// for (const simEl of simpsons) {
+//     let div = document.createElement('div');
+//     div.classList.add('member');
+//
+//     let head = document.createElement('div');
+//     head.innerText = simEl.name + ' ' + simEl.surname + ', age - ' + simEl.age;
+//     head.style.fontWeight = 'bold'
+//     div.appendChild(head);
+//
+//     let info = document.createElement('div');
+//     info.innerText = simEl.info;
+//     div.appendChild(info)
+//
+//     let img = document.createElement('img');
+//     img.src = simEl.photo;
+//     div.appendChild(img);
+//
+//     document.body.appendChild(div);
+// }
+
+// // 9 - Є масив coursesArray котрий лежить в arrays.js (на цей момент ви вже знаєте де він знаходиться)
+// // Цикл в циклі
+// // Створити для кожного елементу масиву свій блок, блок розділити блоками, в яких будуть зберігатись значення окремих властивостей, для властивості modules зробити список з елементами
+// // Приклад структири знаходиться у файлі example.png який лежить в папці з поточним фйлом
+// let coursesArray = [
+//     {
+//         title: 'JavaScript Complex',
+//         monthDuration: 5,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+//     },
+//     {
+//         title: 'Java Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'java core',
+//             'java advanced']
+//     },
+//     {
+//         title: 'Python Complex',
+//         monthDuration: 6,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'python core',
+//             'python advanced']
+//     },
+//     {
+//         title: 'QA Complex',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+//     },
+//     {
+//         title: 'FullStack',
+//         monthDuration: 7,
+//         hourDuration: 909,
+//         modules: ['html',
+//             'css',
+//             'js',
+//             'mysql',
+//             'mongodb',
+//             'react',
+//             'angular',
+//             'aws',
+//             'docker',
+//             'git',
+//             'node.js',
+//             'python',
+//             'java']
+//     },
+//     {
+//         title: 'Frontend',
+//         monthDuration: 4,
+//         hourDuration: 909,
+//         modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+//     }
+// ];
+//
+// for (const element of coursesArray) {
+//
+//     let box1 = document.createElement('div');
+//
+//     let title = document.createElement('div');
+//     title.innerText = element.title
+//     box1.appendChild(title);
+//
+//     let box2 = document.createElement('div');
+//
+//     let monthDuration = document.createElement('div');
+//     monthDuration.innerText = 'Month Duration - ' + element.monthDuration;
+//     monthDuration.style.marginRight = '20px';
+//
+//     let hourDuration = document.createElement('div');
+//     hourDuration.innerText = 'Hour Duration - ' + element.hourDuration;
+//
+//     box2.appendChild(monthDuration);
+//     box2.appendChild(hourDuration);
+//     box2.style.display = 'flex';
+//
+//     box1.appendChild(box2)
+//
+//     let modules = document.createElement('div');
+//     let ol = document.createElement('ol');
+//
+//     for (const item of element.modules) {
+//         let li = document.createElement('li');
+//         li.innerText = item;
+//         ol.appendChild(li);
+//     }
+//
+//     modules.appendChild(ol);
+//     box1.appendChild(modules);
+//     document.body.appendChild(box1);
+//
+// }
+
+//
+// 10 - Створити довільний елемент з id = text та створити кнопку.Використовуючи JavaScript, зробіть так, щоб при натисканні на кнопку зникав елемент з id="text".
+//
+// let div = document.createElement('div');
+// div.id = 'text';
+// document.body.appendChild(div);
+// div.style.width = '100px';
+// div.style.height = '100px';
+// div.style.backgroundColor = '#899bc4';
+//
+// let btn = document.createElement('button');
+// btn.innerText = 'click';
+// document.body.appendChild(btn);
+// btn.onclick = function () {
+//     let element = document.getElementById('text');
+//     element.remove();
+// }
+
+
+//
+//     - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
+//
+// *** Створити 3 інпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
+//     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
+// (Додатковачастина для завдання)
